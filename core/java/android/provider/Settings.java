b5243adfd0b4ce04c8070305b6cece48a666b5e7
Nexus-Evolution <gsxr750dhaj@gmail.com>
2013-01-25 15:24:54 -0800

    Frameworks: customizable slim lockscreen background (1/2)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3006,6 +3006,19 @@ public final class Settings {
         public static final String LOCKSCREEN_BACKGROUND = "lockscreen_background";
 
         /**
+         * Sets the lockscreen background style array helper
+         * 0 = color fill, 1 = custom image, 2 = full transparent, 3 = default background
+         * @hide
+         */
+        public static final String LOCKSCREEN_BACKGROUND_VALUE = "lockscreen_background_VALUE";
+
+        /**
+         * Sets the lockscreen background alpha
+         * @hide
+         */
+        public static final String LOCKSCREEN_ALPHA = "lockscreen_alpha";
+
+        /**
          * Always show the battery status on the lockscreen
          * @hide
          */
