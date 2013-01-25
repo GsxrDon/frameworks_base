d3e458d961c9bcd08c2aad643e1f4128af5673ee
Nexus-Evolution <gsxr750dhaj@gmail.com>
2013-01-25 11:16:04 -0800

    Port Dotted Circle Battery (1/2)
    Fixed the tabs hopefully
    
    Patchset 4
    - fixed last tabs
    
     Add var for dotted circle battery....
    
    next time please pay attention that the vars belong to that commit are not
    in another one ;)
    
    Change-Id: I842929b74116cadbfbbbbf221c9cccc0dc885838

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2768,8 +2768,20 @@ public final class Settings {
          * @hide
          */
         public static final String STATUS_BAR_SIGNAL_TEXT = "status_bar_signal";
+        
+        /**
+        * @hide
+        * Style of Battery
+        * 0 - Icon Only
+        * 1 - Text Only
+        * 2 - Icon Text
+        * 3 - Icon Centered Text
+        * 4 - Icon Circle
+        * 5 - Hide
+        */
+        public static final String STATUSBAR_BATTERY_ICON = "statusbar_battery_icon";
 
-         /**
+        /**
          * Whether to control brightness from status bar
          *
          * @hide
